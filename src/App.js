3e5d2f9f780e67a625edc8c1f901a75f0aeb8f39
manisha sinha <bits.manisha@gmail.com>
2022-10-15 11:30:37 +0200

    function run every single time.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React,{ useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function countInitial(){
+function countInitial() {
   console.log('run function')
-    return 4
+  return 4
 }
 
-
 function App() {
- 
-  const [count,setCount] =useState((countInitial))
- 
-   function decrementCount(){
-    setCount (previousCount => previousCount-1)
-    setCount (previousCount => previousCount-1)
+  const [count, setCount] = useState(() => countInitial())
 
-   }
+  function decrementCount() {
+    setCount((previousCount) => previousCount - 1)
+    setCount((previousCount) => previousCount - 1)
+  }
 
-   function incrementCount(){
-    setCount (previousCount => previousCount+1)
-   }
+  function incrementCount() {
+    setCount((previousCount) => previousCount + 1)
+  }
   return (
-   <>
-   <button onClick={decrementCount}>-</button>
-   <span>{count}</span>
-   <button onClick={incrementCount}>+</button>
-  </>
-  )
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+  );
 }
 
 export default App;
